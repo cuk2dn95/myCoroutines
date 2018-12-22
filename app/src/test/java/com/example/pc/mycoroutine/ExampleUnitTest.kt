8a18af88398d1cbcee83cf9224e1be50a9207c5c
khanh <cuk2dn96@gmail.com>
2018-12-22 22:22:33 +0700

    Init dependencies

---
+++ app/src/test/java/com/example/pc/mycoroutine/ExampleUnitTest.kt
@@ -0,0 +1,21 @@
+package com.example.pc.mycoroutine
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.NonCancellable.isActive
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() = runBlocking{
+
+
+    }
+
+}
